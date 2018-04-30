d072a5752ed98ddf097778ae2ce5ccfeec397173
David Jin <dj256@cornell.edu>
2018-04-30 17:58:33 -0400

    removed mli

--- _build/player.ml
+++ _build/player.ml
@@ -8,24 +8,6 @@ type intelligence = AI | Human
 (* [card] represents a card in the UNO game*)
 type card = {value: int; color: color; effect: effect; id: int}
 
-(* [Player] is a module representing a player in the game *)
-module type Player = sig
-  val id : int
-  (* [name] is the name of the player *)
-  val name : string
-  val intelligence : intelligence
-
-  (* [hand] is a list of cards representing a player's hand *)
-  val hand : card list
-  (* [choose_card] returns the card that will be played by the
-   * player given a top_card and a hand. This function will vary
-   * depending on the type of player: AI or human *)
-  val choose_card : card -> card list -> card
-  (* [cards_left] returns the number of cards left in the
-   * player's hand *)
-  val cards_left : card list -> int
-end
-
 let rec does_wild4_exist hand =
   match hand with
   | [] -> None
